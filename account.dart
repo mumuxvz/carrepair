429a1d462fb66964fa4d614987a0ba835db2d437
mumuxvz <90172083+mumuxvz@users.noreply.github.com>
2022-05-25 12:48:17 +0530

    Add files via upload

---
+++ account.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class account extends StatefulWidget {
+  const account({Key? key}) : super(key: key);
+
+  @override
+  _accountState createState() => _accountState();
+}
+
+class _accountState extends State<account> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
---
+++ sos.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/material.dart';
+
+class sos extends StatefulWidget {
+  const sos({Key? key}) : super(key: key);
+
+  @override
+  _sosState createState() => _sosState();
+}
+
+class _sosState extends State<sos> {
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}

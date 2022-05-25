fcd9a8c06fae9ac59207827b3af505e89c3ef047
mumuxvz <90172083+mumuxvz@users.noreply.github.com>
2022-05-25 09:18:03 +0530

    Add files via upload

---
+++ lib/main.dart
@@ -0,0 +1,29 @@
+import 'package:carrepair/loginscreen.dart';
+import 'package:flutter/material.dart';
+import 'package:firebase_core/firebase_core.dart';
+Future<void> main() async{
+  WidgetsFlutterBinding.ensureInitialized();
+  await Firebase.initializeApp();
+  runApp(MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  // This widget is the root of your application.
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      title: 'Flutter Demo',
+      theme: ThemeData(
+
+        colorScheme: ColorScheme.fromSwatch().copyWith(
+
+          primary: Colors.black,
+        ),
+      ),
+      home:loginscreen(),
+    );
+  }
+}
